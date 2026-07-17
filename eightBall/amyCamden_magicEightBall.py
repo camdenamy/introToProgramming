03fa9724fb2e142822c0fe4da179ef8409914cab
Camden Amy <88038970+camdenamy@users.noreply.github.com>
2026-07-17 09:28:02 -0700

    answers list change
    reduced number of responses from 20 to 8.

--- eightBall/amyCamden_magicEightBall.py
+++ eightBall/amyCamden_magicEightBall.py
@@ -2,28 +2,17 @@ import random
 
 def responses():
     answers = [
-        "It is certain.",
         "It is decidedly so.",
         "Without a doubt.",
-        "Yes definitely.",
         "You may rely on it.",
         "As I see it, yes.",
-        "Most likely.",
-        "Outlook good.",
-        "Yes.",
         "Signs point to yes.",
         "Reply hazy, try again.",
-        "Ask again later.",
-        "Better not tell you now.",
         "Cannot predict now.",
-        "Concentrate and ask again.",
         "Don't count on it.",
-        "My reply is no.",
         "My sources say no.",
-        "Outlook not so good.",
-        "Very doubtful.",
     ]
-    random_number = random.randint(1, 20)
+    random_number = random.randint(1, 8)
     return answers[random_number - 1]
 
 def main():
